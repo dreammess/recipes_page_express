715b9eb0f388cff2b8b4f6196009a16965dffd44
Magdalena Stevens <magdalena.r.stevens@gmail.com>
2017-12-15 23:54:15 +0100

    Add template rendering.
    App now renders dummy templates.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.send('This will be a home page!');
+	res.render('index');
 });
 
 module.exports = router;
--- routes/recipes.js
+++ routes/recipes.js
@@ -2,27 +2,32 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.send('This will be a recipes page!');
+	res.locals.title = 'This will be a recipes page!';
+	res.render('recipes');
 });
 
 //Soups
 
 router.get('/soups', (req, res) => {
-	res.send('This is where the soups will go!');
+	res.locals.title = 'This is where the soups will go!';
+	res.render('recipe_menu');
 });
 
 router.get('/soups/1', (req, res) => {
-	res.send('This will be a carrot soup recipe');
+	res.locals.title = 'This will be a carrot soup recipe';
+	res.render('recipe');
 });
 
 //Pasta
-
+ 
 router.get('/pasta', (req, res) => {
-	res.send('This is where the pastas will go!');
+	res.locals.title = 'This is where the pastas will go!';
+	res.render('recipe_menu');
 });
 
 router.get('/pasta/1', (req, res) => {
-	res.send('This will be a parsley-lemon pasta recipe');
+	res.locals.title = 'This will be a parsley-lemon pasta recipe';
+	res.render('recipe');
 });
 
 
