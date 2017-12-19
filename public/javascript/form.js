4c5660b74ece823c503a0601c54ccf0d9202fec1
Magdalena Stevens <magdalena.r.stevens@gmail.com>
2017-12-19 11:14:58 +0100

    Add edit/delete functionality to Add Recipe page.
    Code is a bit chaotic, but functional. Should switch over to a FE
    library? Maybe just refactor.`

--- public/javascript/form.js
+++ public/javascript/form.js
@@ -8,7 +8,8 @@ document.addEventListener('DOMContentLoaded', function() {
 	function arrayUpdate(myArray, input, list) {
 		var li = document.createElement('li');
 		myArray.push(input.value);
-		li.innerHTML = input.value;
+		li.innerHTML = input.value + "<button type='button' class='delete-button'>Delete</button>" 
+		+ "<button type='button' class='edit-button'>Edit</button>";
 		list.append(li);
 		input.value = "";
 	}
@@ -18,6 +19,31 @@ document.addEventListener('DOMContentLoaded', function() {
     var ingredientButton = document.getElementById('ingredient-btn');
 	var ingredientList = document.getElementById('ingredient-list');
 	var ingredientInput = document.getElementById('ingredient-input');
+
+	//Remove & Edit Button
+
+	ingredientList.addEventListener('click', (e) => {
+		if (e.target.className == 'delete-button') {
+			let li = event.target.parentNode;
+			ingredientList.removeChild(li);
+			var liText = li.textContent.replace("DeleteEdit", "");
+			ingredients = ingredients.filter(x => x !== liText);
+		} else if (e.target.className == 'edit-button') {
+			let li = event.target.parentNode;
+			var liText = li.textContent.replace("DeleteEdit", "");
+			li.innerHTML = "<input class='save-edit' value='" + liText + "'></input>";
+			document.getElementsByClassName('save-edit')[0].addEventListener('keyup', (e) => {
+				if (e.keyCode == 13) {
+					ingredients = ingredients.map(x => (x == liText) ? e.target.value:x);
+					console.log(e.target.value);
+					li.innerHTML = e.target.value +
+						"<button type='button' class='delete-button'>Delete</button>" +
+						"<button type='button' class='edit-button'>Edit</button>";
+				}
+			});
+		} 
+	});
+
 
 	ingredientButton.addEventListener('click', () => {
 		arrayUpdate(ingredients, ingredientInput, ingredientList);
@@ -35,10 +61,12 @@ document.addEventListener('DOMContentLoaded', function() {
 	var instructionList = document.getElementById('instruction-list');
 	var instructionInput = document.getElementById('instruction-input');
 
+	// Use the button(p tag) to accept input
 	instructionButton.addEventListener('click', () => {
 		arrayUpdate(instructions, instructionInput, instructionList);
 	});
 
+	// Use the enter key to accept input
 	instructionInput.addEventListener('keyup', function (e) {
     	if (e.keyCode == 13) {
        		arrayUpdate(instructions, instructionInput, instructionList);
@@ -48,6 +76,8 @@ document.addEventListener('DOMContentLoaded', function() {
 }, false);
 
 
+// Bad name as it is not actually validating the form,
+// but rather adding content to the invisible inputs.
 function validateForm() {
 	ingredients = JSON.stringify(ingredients);
 	instructions = JSON.stringify(instructions);
